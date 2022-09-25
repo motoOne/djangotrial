70a776667b43feeea3687482e4af46b035656513
prajwalahetti <parjwal.ahetti@gmail.com>
2022-09-25 14:09:48 +0530

    first

--- testdjango/settings.py
+++ testdjango/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'static'
